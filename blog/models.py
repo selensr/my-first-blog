99f77806fc7bf606d11803a02e8ca5895ac2b5db
selensr <ksmmevzuat@gmail.com>
2019-10-26 15:04:05 +0300

    bug fixes

--- blog/models.py
+++ blog/models.py
@@ -7,13 +7,13 @@ class Post(models.Model):
 	text = models.TextField()
 	created_date = models.DateTimeField(
 		default = timezone.now)
-	published_date = models. DateTimeField(
+	published_date = models.DateTimeField(
 		blank=True, null=True)
 
-def publish(self):
-	self.published_date = timezone.now()
-	self.save()
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
 
-def __str__(self):
-	return self.title
+	def __str__(self):
+		return self.title
 
